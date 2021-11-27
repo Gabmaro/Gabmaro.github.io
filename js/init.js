3f6abe1d0fb17a518bdbc0bc0073558d3661a516
Gabmaro <93953401+Gabmaro@users.noreply.github.com>
2021-11-26 21:03:58 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,11 +8,11 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyCZ9pJY6qIl_zhL31XgvQNM1PBtdqcUK0E",
-  authDomain: "gabchat-26b95.firebaseapp.com",
-  projectId: "gabchat-26b95",
-  storageBucket: "gabchat-26b95.appspot.com",
-  messagingSenderId: "1044747948828",
-  appId: "1:1044747948828:web:d35fbf15f81883229620d9",
-  measurementId: "G-DVX3564H8K"
+  apiKey: "AIzaSyCCfe345ly5Q7_kSGdmWhLck16FyAZE3es",
+  authDomain: "gabfin-1dead.firebaseapp.com",
+  projectId: "gabfin-1dead",
+  storageBucket: "gabfin-1dead.appspot.com",
+  messagingSenderId: "840417997183",
+  appId: "1:840417997183:web:550f183ed4b029675aaa4a",
+  measurementId: "G-JT97313XG7"
 });
