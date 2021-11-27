a70ceb1620a9d92db827c3fd87567606830ed45f
Gabmaro <93953401+Gabmaro@users.noreply.github.com>
2021-11-26 20:40:53 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -7,12 +7,12 @@
  * quedar igual que en tu
  * servidor. */
 // @ts-ignore
-firebase.initializeApp({
-  piKey: "AIzaSyCZ9pJY6qIl_zhL31XgvQNM1PBtdqcUK0E",
+const firebaseConfig = {
+  apiKey: "AIzaSyCZ9pJY6qIl_zhL31XgvQNM1PBtdqcUK0E",
   authDomain: "gabchat-26b95.firebaseapp.com",
   projectId: "gabchat-26b95",
   storageBucket: "gabchat-26b95.appspot.com",
   messagingSenderId: "1044747948828",
   appId: "1:1044747948828:web:d35fbf15f81883229620d9",
   measurementId: "G-DVX3564H8K"
-});
+};
