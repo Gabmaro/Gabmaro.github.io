064080b7169ecb247c86d0b98fa745fd0508a419
Gabmaro <93953401+Gabmaro@users.noreply.github.com>
2021-11-27 02:16:10 -0600

    Update mi-nav.js

--- cmp/mi-nav.js
+++ cmp/mi-nav.js
@@ -58,7 +58,7 @@ class MiNav extends HTMLElement {
                 <a href="usuarios.html">Usuarios</a>
               </li>
               <li>
-                <a href="/galeria/gallery.html">Galeria</a>
+                <a href="/galeria/index.html">Galeria</a>
               </li>`;
           }
           this.ul.innerHTML += html;
